b73bdf0fcdfa10d6e3ac8ca990e8599d4cd2e95f
dfvas <166792689+dfvas@users.noreply.github.com>
2026-04-30 18:44:33 +0900

    Implement average calculation and percentage output

--- 수학/4344/평균은 넘겠지.cpp
+++ 수학/4344/평균은 넘겠지.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+	int n, t;
+	int a[1001];
+	int average;
+	double res;
+  
+	cin >> n;
+
+	while (n--) {
+		cin >> t;
+		average = 0;
+		res = 0;
+    
+		for (int i = 0; i < t; i++) {
+			cin >> a[i];
+			average += a[i];
+		}
+		average /= t;
+
+		for (int i = 0; i < t; i++) {
+			if (a[i] > average) res++;
+		}
+
+		cout << fixed;
+		cout.precision(3);
+		cout << res / t * 100 << "% \n";
+	}
+}
