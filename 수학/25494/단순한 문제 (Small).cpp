621da26268f4cc93103fca2eb74e3af7c4c6cfbb
dfvas <166792689+dfvas@users.noreply.github.com>
2026-05-02 00:01:17 +0900

    Update 단순한 문제 (Small).cpp

--- 수학/25494/단순한 문제 (Small).cpp
+++ 수학/25494/단순한 문제 (Small).cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int T; cin >> T;
+	while (T--) {
+		int cnt = 0;
+		int a, b, c; cin >> a >> b >> c;
+		for (int x = 1; x <= a; x++) {
+			for (int y = 1; y <= b; y++) {
+				for (int z = 1; z <= c; z++) {
+					int X = x % y, Y = y % z, Z = z % x;
+					if (X == Y && Y == Z) cnt++;
+				}
+			}
+		}
+		cout << cnt << '\n';
+	}
+}
