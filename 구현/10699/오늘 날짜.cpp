096ef16a11bbcf84fd3941fe560cf9497b9ca292
dfvas <166792689+dfvas@users.noreply.github.com>
2026-05-03 00:30:49 +0900

    Create 오늘 날짜.cpp

---
+++ 구현/10699/오늘 날짜.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+	time_t timer = time(NULL); 
+	struct tm* t = localtime(&timer);
+
+	cout << t->tm_year + 1900 << "-";
+
+	cout.width(2); 
+	cout.fill('0'); 
+
+	cout << t->tm_mon + 1 << "-" << t->tm_mday << endl;
+
+	return 0;
+}
