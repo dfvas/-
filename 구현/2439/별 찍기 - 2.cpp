60326e9b9a6c4b7c42436cc5b070cfc941e5d7dc
dfvas <166792689+dfvas@users.noreply.github.com>
2026-05-02 23:40:43 +0900

    Create 별 찍기 - 2.cpp

---
+++ 구현/2439/별 찍기 - 2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	
+	int t;
+	cin >> t;
+	
+	for(int i = 1; i <= t; i++) {
+		for(int k = 0; k < t - i; k++) {
+			cout << " ";
+		}
+		for(int k = 0; k < i; k++) {
+			cout << "*";
+		}
+		cout << '\n';
+	}
+	
+	return 0;
+}
