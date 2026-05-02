5cbebe8f052bca0b58a9787fac2329fc8049f1d6
dfvas <166792689+dfvas@users.noreply.github.com>
2026-05-02 22:15:08 +0900

    Create 음계.cpp

---
+++ 구현/2920/음계.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num[8], judge[8] = {0};
+    for (int i = 0; i < 8; i++) {
+        cin >> num[i];
+        if (i != 0) {
+            if (num[i - 1] < num[i]) judge[i] = 1;  
+            if (num[i - 1] > num[i]) judge[i] = 0;  
+        }
+    }
+    for (int i = 2; i < 8; i++) {
+        if (judge[i - 1] != judge[i]) {
+            cout << "mixed";
+            return 0;
+        }
+    }
+    if (judge[1]) cout << "ascending";
+    else cout << "descending";
+    return 0;
+}
